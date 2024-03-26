00dd7c9c4d3ff3c5651a95fc371ee3ad623c2663
Muhammad Usman Asif <usmanphilco@gmail.com>
2024-03-26 05:14:47 +0500

    My second commit

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulation! you guessed right number");
+}
+else {
+    console.log("You guessed wrong number");
+}
